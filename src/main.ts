2ba7f91fddad7ecb10e01636ed9b5239b6958061
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-06 13:46:52 -0300

    feat(bootstrap): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from '@/app.module';
 
@@ -11,6 +12,15 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Formatch API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
