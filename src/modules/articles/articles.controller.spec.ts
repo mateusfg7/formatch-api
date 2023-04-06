fdf65edfd3953670447c11b749122f67edaaafe1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-04-06 10:15:37 -0300

    test(articles.controller): fix providers

--- src/modules/articles/articles.controller.spec.ts
+++ src/modules/articles/articles.controller.spec.ts
@@ -1,6 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+
+import { PrismaService } from '@/database/prisma.service';
+import { ArticlesRepository } from '@/repositories/articles-repository';
+
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
+
+const repository = {
+  create: jest
+    .fn()
+    .mockImplementation(
+      ({
+        banner_url,
+        content,
+        slug,
+        sources,
+        title,
+      }: {
+        title: string;
+        slug: string;
+        banner_url: string;
+        sources: string;
+        content: string;
+      }) => {
+        return {
+          id: faker.datatype.uuid(),
+          slug,
+          title,
+          banner_url,
+          sources,
+          content,
+          createdAt: faker.date.recent(0),
+          updatedAt: faker.date.recent(0),
+        };
+      },
+    ),
+};
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
@@ -8,7 +44,14 @@ describe('ArticlesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
-      providers: [ArticlesService],
+      providers: [
+        ArticlesService,
+        PrismaService,
+        {
+          provide: ArticlesRepository,
+          useValue: repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<ArticlesController>(ArticlesController);
