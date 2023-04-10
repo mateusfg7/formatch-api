f7d4763b05c611425b143c03bfdc870306f8996e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-04-10 11:03:08 -0300

    chore(jest): create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/$1',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  detectOpenHandles: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../docs/coverage',
+  coverageReporters: ['html-spa', 'text-summary'],
+}
+
+export default config
