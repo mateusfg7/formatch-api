bc5b089cad52452e5c2b3aff3e88e92053a04b88
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-04-12 14:30:24 -0300

    test(articles.controller): add tests for `create()` method

--- src/modules/articles/articles.controller.spec.ts
+++ src/modules/articles/articles.controller.spec.ts
@@ -7,39 +7,9 @@ import { ArticlesRepository } from '@/repositories/articles-repository'
 import { ArticlesController } from './articles.controller'
 import { ArticlesService } from './articles.service'
 
-const repository = {
-  create: jest
-    .fn()
-    .mockImplementation(
-      ({
-        banner_url,
-        content,
-        slug,
-        sources,
-        title,
-      }: {
-        title: string
-        slug: string
-        banner_url: string
-        sources: string
-        content: string
-      }) => {
-        return {
-          id: faker.datatype.uuid(),
-          slug,
-          title,
-          banner_url,
-          sources,
-          content,
-          createdAt: faker.date.recent(0),
-          updatedAt: faker.date.recent(0),
-        }
-      },
-    ),
-}
-
-describe('ArticlesController', () => {
-  let controller: ArticlesController
+describe('Articles Controller', () => {
+  let articlesController: ArticlesController
+  let articlesService: ArticlesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -49,15 +19,49 @@ describe('ArticlesController', () => {
         PrismaService,
         {
           provide: ArticlesRepository,
-          useValue: repository,
+          useValue: {
+            create: jest.fn().mockImplementation(() => 0),
+          },
         },
       ],
     }).compile()
 
-    controller = module.get<ArticlesController>(ArticlesController)
+    articlesController = module.get<ArticlesController>(ArticlesController)
+    articlesService = module.get<ArticlesService>(ArticlesService)
   })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined()
+    expect(articlesController).toBeDefined()
+    expect(articlesService).toBeDefined()
+  })
+
+  describe('create', () => {
+    it('should return an article', async () => {
+      const articlePayload = {
+        title: 'As vantagens do Reboco',
+        slug: 'as-vantagens-do-reboco',
+        banner_url: faker.image.abstract(),
+        content: faker.lorem.text(),
+        sources: [
+          faker.internet.url(),
+          faker.internet.url(),
+          ` ${faker.internet.url()}`,
+        ],
+      }
+      const createdArticle = {
+        ...articlePayload,
+        id: faker.datatype.uuid(),
+        sources: String(articlePayload.sources),
+        createdAt: faker.date.recent(0),
+        updatedAt: faker.date.recent(0),
+      }
+
+      jest.spyOn(articlesService, 'create').mockResolvedValue(createdArticle)
+
+      const result = await articlesController.create(articlePayload)
+
+      expect(result).toEqual(createdArticle)
+      expect(articlesService.create).toHaveBeenCalledWith(articlePayload)
+    })
   })
 })
