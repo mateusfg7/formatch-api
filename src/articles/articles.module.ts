842d33cf6fbf05f0bfd1d38e5e57efc7a8a009df
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-04-18 10:28:41 -0300

    feat(articles.module): provide new `PrismaService`

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common'
 import { ArticlesController } from './articles.controller'
 import { ArticlesService } from './articles.service'
 
-import { PrismaService } from '@/database/prisma.service'
+import { PrismaService } from '@/common/services/prisma.service'
 
 import { ArticlesRepository } from '@/repositories/articles-repository'
 import { PrismaArticlesRepositoryService } from '@/repositories/prisma/prisma-articles-repository.service'
